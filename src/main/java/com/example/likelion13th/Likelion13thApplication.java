9a8de84791dce317af79aad6c7b9f8c6a19b4930
brian <kimchanjoong54@gmail.com>
2025-07-16 20:49:30 +0900

    week16 code added

--- src/main/java/com/example/likelion13th/Likelion13thApplication.java
+++ src/main/java/com/example/likelion13th/Likelion13thApplication.java
@@ -2,8 +2,10 @@ package com.example.likelion13th;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Enables JPA Auditing to automatically populate createdAt and updatedAt fields
 public class Likelion13thApplication {
 
 	public static void main(String[] args) {
